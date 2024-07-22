8b3f748dcb40a67e2ae446092e795e18780b3517
Nikhil Kumar <144894299+nikhil73521@users.noreply.github.com>
2024-07-22 05:21:57 +0000

    Next Lecture

---
+++ 01_Basics/variable.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "kumarnikhil735214@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState;
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+console.log();
