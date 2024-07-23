8cb7d7d2e49a6f20163b1bebdf9df0c7ea78e43a
Nikhil Kumar <144894299+nikhil73521@users.noreply.github.com>
2024-07-23 12:30:39 +0000

    String

---
+++ 01_Basics/string.js
@@ -0,0 +1,17 @@
+const name = "Nikhil"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+// This is not good because this types of code old 
+
+console.log(`Hello my name is `);
+const gameName = new String(`nikhil`)
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf(`t`));
