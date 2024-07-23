808ba376c208caf72bf30b7fbc4ee71f12ac5c76
Nikhil Kumar <144894299+nikhil73521@users.noreply.github.com>
2024-07-23 11:14:44 +0000

    Comparision in DataTypes

---
+++ 01_Basics/comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+/* These comparision are avoided Because this comparisions always confused.Always use simple and readable code. */
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strictly check datatypes
+
+console.log("2" ===2);
