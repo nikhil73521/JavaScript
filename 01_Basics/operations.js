9c770434f3f3243558bb53ad8a35821e8558df78
Nikhil Kumar <144894299+nikhil73521@users.noreply.github.com>
2024-07-23 11:01:26 +0000

    Operators

---
+++ 01_Basics/operations.js
@@ -0,0 +1,45 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2); //Addition
+// console.log(2-2); //Subtract
+// console.log(2*2); //Multiplication
+// console.log(2**2); //Power
+// console.log(2/2); //Divide
+// console.log(2%2); //Modulo
+
+let str1 = "hello"
+let str2 = " Nikhil"
+ 
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log("1" + 2 + "2");
+// console.log(1 + 2 + "2");
+// console.log(3 + 4 * 5 % 3); // This Code is not good, Anywhere don't write this types of code.
+
+// console.log((3 + 4) * 5 % 3); // This way to write the code is good.
+
+// console.log(+true); // Because it is boolean
+
+// console.log(true+); // this code is wrong 
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100
+gameCounter++;
+// Both are same  ++gameCounter;
+
+
+console.log(gameCounter);
+
+
