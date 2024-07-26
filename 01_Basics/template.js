decf9dd574188daf633a961d9de192167fd12fed
Nikhil Kumar <144894299+nikhil73521@users.noreply.github.com>
2024-07-26 07:17:38 +0000

    template

--- 01_Basics/template.js
+++ 01_Basics/template.js
@@ -1,3 +1,27 @@
+
+     // ***************Operator in Js*****************
+// Arithmetic[+ , - , * , / , % , **]
+// Unary[++ , --]
+// Assignment[= , += , -= , *= /= , %= etc.]
+// Comparison 
+// Logical 
+
 let a = 10;
-let b = 20;
-console.log(a + b);
+let b = 5;
+console.log(a + b); // Ans. 15
+console.log(a - b);  //Ans. 5
+console.log(a * b); //Ans. 50
+console.log(a / b); //Ans. 2
+console.log(a % b); //Ans. 0
+console.log(a ** b); //Ans. 100000
+
+console.log(a++);  // Ans. 10
+console.log(++a); // Ans. 12
+
+console.log(+a);
+
+
+
+
+
+
