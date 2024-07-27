2f13449200b1455b1165e9bf5695623fd15260aa
Nikhil Kumar <144894299+nikhil73521@users.noreply.github.com>
2024-07-27 06:38:14 +0000

    Arrays

--- 02_Basics/arrays.js
+++ 02_Basics/arrays.js
@@ -1,10 +1,10 @@
 // array
 
-const myArr = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-const myHeros = ["Shaktiman", "Spiderman"]
+// const myArr = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+// const myHeros = ["Shaktiman", "Spiderman"]
 
-const myArr2 = new Array(1, 2, 3, 4)
-console.log(myArr[0]);
+// const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[0]);
 
 // Array methods
 
@@ -18,18 +18,44 @@ myArr.unshift(9) // add to start
 myArr.shift() // delete from start & return it
 */
 
- /* console.log(myArr.includes(2)); //  search for a value
+/* console.log(myArr.includes(2)); //  search for a value
 console.log(myArr.indexOf(3)); // returns index of something
 */
 
-const newArr = myArr.join()
-console.log(myArr);
-console.log(newArr);
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);
 
 // slice, splice
 
-console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+// let colors = ["red", "yellow", "blue", "orange", "pink", "White"];
+// console.log(colors.slice(2));
 
-console.log(myArr);
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("Nikhil"));
+console.log(Array.from("Nikhil"));
+
+console.log(Array.from({ name: "Nikhil" })); //interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
